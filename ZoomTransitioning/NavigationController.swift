6e5f3eab58b3459883daa9119b8f81da896c219c
WorldDownTown <WorldDownTown@gmail.com>
2016-07-19 01:37:55 +0900

    compatible Carthage

--- ZoomTransitioning/NavigationController.swift
+++ ZoomTransitioning/NavigationController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import ZoomTransitioning
 
 class NavigationController: UINavigationController {
 
---
+++ ZoomTransitioningFramework/ZoomTransitioningFramework.h
@@ -0,0 +1,19 @@
+//
+//  ZoomTransitioningFramework.h
+//  ZoomTransitioningFramework
+//
+//  Created by shoji on 7/19/16.
+//  Copyright © 2016 com.shoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZoomTransitioningFramework.
+FOUNDATION_EXPORT double ZoomTransitioningFrameworkVersionNumber;
+
+//! Project version string for ZoomTransitioningFramework.
+FOUNDATION_EXPORT const unsigned char ZoomTransitioningFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZoomTransitioningFramework/PublicHeader.h>
+
+
